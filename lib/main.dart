9f1203be1a79aa7a8cbfb08affa85729a1e2564e
sarojyadav <sarojyadav88505@gmail.com>
2020-08-29 00:12:04 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //void main is main function
+  //runApp is a function
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      debugShowCheckedModeBanner: false,
+      title: "day1",
+      home: Scaffold(
+        drawer: Drawer(),
+        appBar: AppBar(
+          title: Text("day1"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text("Day1 code"),
+        ),
+      ),
+    ),
+  );
+}
